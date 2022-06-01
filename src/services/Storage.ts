747f1f00468ce70a5b82265fa611c2f705b9afe4
Mateus Henrique <mateushenriquechaves1@gmail.com>
2022-06-01 01:59:28 -0300

    feat: create service to manage storage data

---
+++ src/services/Storage.ts
@@ -0,0 +1,11 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const tokenStoragePath = '@token'
+
+export async function getToken(): Promise<string | null> {
+    return AsyncStorage.getItem(tokenStoragePath)
+}
+
+export async function setToken(token: string): Promise<void> {
+    return AsyncStorage.setItem(tokenStoragePath, token)
+}
