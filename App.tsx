a3371a69d5a72a3776e3c57a99111688b2ee3525
Mateus Henrique <mateushenriquechaves1@gmail.com>
2022-06-01 01:58:33 -0300

    feat: load fonts before launch app

--- App.tsx
+++ App.tsx
@@ -1,14 +1,29 @@
 
-import { StyleSheet, Text, View } from 'react-native';
-import { Routes } from './src/Routes/index';
-
-
+import Routes from './src/Routes/index';
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts, Inter_400Regular, Inter_500Medium, Inter_700Bold } from '@expo-google-fonts/inter';
+import { ActivityIndicator } from 'react-native';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_700Bold
+  })
+
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator />
+  }
+
+  (async () => {
+    await SplashScreen.hideAsync();
+  })()
+
   return (
-
-<Routes />
-
+    <Routes />
   );
+
 }
 
